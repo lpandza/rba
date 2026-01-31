c56b79d424f88b71ec2b8503477a8669f93977e7
lpandza <lukapandza@hotmail.com>
2026-01-31 16:49:02 +0100

    Initial commit

---
+++ src/main/java/com/lpandza/rba/RbaApplication.java
@@ -0,0 +1,13 @@
+package com.lpandza.rba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbaApplication.class, args);
+	}
+
+}
